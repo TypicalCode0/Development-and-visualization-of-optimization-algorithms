2bf929ee41bf45c0b6ce584bf43c84b8ef50c34d
Nithon <g4zizullinn@yandex.ru>
2024-04-10 20:49:59 +0300

    update ui and setup.py and FunctionObj_v1.py

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,10 @@
-from locale import normalize
-from re import A
 import sys
 
-
-from PyQt6 import uic, QtCore, QtGui
+from PyQt6 import uic
 from PyQt6.QtWidgets import *
-from matplotlib import projections
-from FunctionObj_v1 import FunctionObj as func
+from sympy import SympifyError
+
+from FunctionObj_v1 import FunctionObj as Func
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -14,40 +12,41 @@ from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 
 
-
-
 class VisualisationApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('ui\\main.ui', self)
+        uic.loadUi("ui/main.ui", self)
         self.setWindowTitle("Visualization of optimization algorithms")
 
         self.constraints = []
+        self.algorithms = ["Gradient Descent", "1"]
+        self.curr_alg = 0
 
-        self.comboBox_choose_alg = self.findChild(QComboBox, "comboBox_choose_alg")
-        self.textEdit_expression = self.findChild(QLineEdit, "lineEdit_expression")
-        self.lineEdit_universe_left = self.findChild(QLineEdit, "lineEdit_universe_left")
-        self.lineEdit_universe_right = self.findChild(QLineEdit, "lineEdit_universe_right")
-        self.lineEdit_new_constraints = self.findChild(QLineEdit, "lineEdit_new_constraints")
-        self.listWidget_constraints = self.findChild(QListWidget, "listWidget_constraints")
-        self.pushButton_add_constraint = self.findChild(QPushButton, "pushButton_add_constraint")
-        self.pushButton_delete_constraint = self.findChild(QPushButton, "pushButton_delete_constraint")
-        self.pushButton_clear_constraints = self.findChild(QPushButton, "pushButton_clear_constraints")
-        self.pushButton_ready = self.findChild(QPushButton, "pushButton_ready")
-        self.pushButton_step = self.findChild(QPushButton, "pushButton_step")
-        self.pushButton_steps = self.findChild(QPushButton, "pushButton_steps")
-        self.pushButton_exit = self.findChild(QPushButton, "pushButton_exit")
-
-        self.widget_graphics = self.findChild(QWidget, "widget_graphics")
         self.graph_layout = QVBoxLayout(self.widget_graphics)
-        self.wait()
+        for i in self.algorithms:
+            self.comboBox_choose_alg.addItem(i)
+        self.comboBox_choose_alg.currentIndexChanged.connect(self.change_status_constraints_widgets)
 
-    def wait(self):
+        self.pushButton_stop.hide()
         self.pushButton_exit.clicked.connect(self.close_program)
         self.pushButton_add_constraint.clicked.connect(self.add_constraint)
         self.pushButton_delete_constraint.clicked.connect(self.delete_constraint)
         self.pushButton_clear_constraints.clicked.connect(self.clear_constraints)
-        self.pushButton_ready.clicked.connect(self.get_data)
+        self.pushButton_ready.clicked.connect(self.start_algorithms)
+        self.pushButton_stop.clicked.connect(self.stop_algorithms)
+
+    def change_status_constraints_widgets(self):
+        current = int(self.comboBox_choose_alg.currentIndex())
+        if current == 0:
+            self.listWidget_constraints.setEnabled(False)
+            self.pushButton_add_constraint.setEnabled(False)
+            self.pushButton_delete_constraint.setEnabled(False)
+            self.lineEdit_new_constraints.setEnabled(False)
+        else:
+            self.listWidget_constraints.setEnabled(True)
+            self.pushButton_add_constraint.setEnabled(True)
+            self.pushButton_delete_constraint.setEnabled(True)
+            self.lineEdit_new_constraints.setEnabled(True)
 
     def check_errors(self):
         pass
@@ -55,26 +54,45 @@ class VisualisationApp(QMainWindow):
     def add_constraint(self):
         constraint = self.lineEdit_new_constraints.text()
         self.constraints.append(constraint)
-        print(self.constraints)
+        self.listWidget_constraints.addItem(constraint)
         self.lineEdit_new_constraints.clear()
-        self.update_list_constraints()
 
     def delete_constraint(self):
-        pass
+        curr_item = self.listWidget_constraints.takeItem(self.listWidget_constraints.currentRow())
+        del curr_item
 
     def clear_constraints(self):
         self.constraints.clear()
-        self.update_list_constraints()
+        self.listWidget_constraints.clear()
 
-    def update_list_constraints(self):
-        pass
+    def start_algorithms(self):
+        self.pushButton_ready.hide()
+        self.pushButton_stop.show()
+        self.pushButton_step.setEnabled(True)
+        self.pushButton_steps.setEnabled(True)
+        self.get_data()
+
+    def stop_algorithms(self):
+        self.pushButton_stop.hide()
+        self.pushButton_ready.show()
+        self.pushButton_step.setEnabled(False)
+        self.pushButton_steps.setEnabled(False)
 
     def get_data(self):
-        print("function is", self.textEdit_expression.text())
+        expression = self.textEdit_expression.toPlainText()
+        print("function is", expression)
         print(f"limitations are: {self.constraints}")
         print(f"universe is: ({self.lineEdit_universe_left.text()};{self.lineEdit_universe_right.text()})")
-        f = func(self.textEdit_expression.text())
-        f.add_border(int(self.lineEdit_universe_left.text()), int(self.lineEdit_universe_right.text()))
+        try:
+            f = Func(expression)
+        except SympifyError:
+            print("Sympify Error")
+            return
+        try:
+            f.add_border(int(self.lineEdit_universe_left.text()), int(self.lineEdit_universe_right.text()))
+        except ValueError:
+            print("ValueError")
+            return
         for limit in self.constraints:
             f.add_constraint(limit)
         self.draw(f)
@@ -82,18 +100,18 @@ class VisualisationApp(QMainWindow):
     def close_program(self):
         sys.exit()
 
-    def draw(self, f):
-        if len(f.variables) == 1:
-            VisualisationApp.two_dimensional(self, f)
-        elif len(f.variables) == 2:
-            VisualisationApp.three_dimensional(self, f)
-
     def clear_layout(self):
         while self.graph_layout.count():
             item = self.graph_layout.takeAt(0)
             widget = item.widget()
             if widget:
                 widget.deleteLater()
+
+    def draw(self, f):
+        if len(f.variables) == 1:
+            VisualisationApp.two_dimensional(self, f)
+        elif len(f.variables) == 2:
+            VisualisationApp.three_dimensional(self, f)
 
     def two_dimensional(self, f):
         size_pic, start, end = 100, f.border[0], f.border[1]
@@ -103,7 +121,7 @@ class VisualisationApp(QMainWindow):
         while i < end:
             rez = f.solve({f.variables[0]: i})
             i += step
-            if (rez is None):
+            if rez is None:
                 continue
             else:
                 x.append(i)
@@ -128,7 +146,7 @@ class VisualisationApp(QMainWindow):
         for i in range(len(X)):
             for j in range(len(X[0])):
                 z = f.solve({f.variables[0]: X[i][j], f.variables[1]: Y[i][j]})
-                if (z is None):
+                if z is None:
                     Z = np.ma.masked_where((X == X[i][j]) & (Y == Y[i][j]), Z)
                 else:
                     Z[i][j] = z
